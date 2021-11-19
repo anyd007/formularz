4c7884dc99cab13d138c3fb5f08e64e68c437e4d
Andrzej Gębala <gebala.andrzej1@gmial.com>
2021-11-19 13:44:16 +0100

    funkcjonalności w js

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 # formularz
 
 Tworzę formularz z dwoma inputami, texterą i buttonem wyślij, szkielet html i style css
+W JS nadałem listenera na buton, oraz określiłem zabezpieczenia przy błędnie wypełnionych inputach
--- script.js
+++ script.js
@@ -1,2 +1,26 @@
+const inputName = document.querySelector(".name-input");
+const inputEmail = document.querySelector(".email-input");
+const inputs = document.querySelectorAll("form input");
+const textera = document.querySelector(".text-feld");
+const sendBtn = document.querySelector(".send-btn");
+const warining = document.querySelector(".warning");
+let nameLetters = /^[a-zA-Z\s]*$/;
 
-// const sendBtn = document.querySelector('.send-btn');
+const sendListener = () => {
+  if (inputName.value.match(nameLetters)) {
+    inputs.forEach((input) => {
+      if (input.value === "") {
+        input.style.backgroundColor = "red";
+        warining.textContent = 'pola "imię" oraz "e-mail" są obowiązkowe';
+      } else {
+        input.style.backgroundColor = "white";
+        warining.textContent = "";
+      }
+    });
+  } else {
+    inputName.style.backgroundColor = "red";
+    warining.textContent = 'pole "imię" zawira niedozwolone znaki';
+  }
+};
+
+sendBtn.addEventListener("click", sendListener);
