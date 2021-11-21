299472ae3f22dd8e74829c1067678a14d078433f
Andrzej Gębala <gebala.andrzej1@gmial.com>
2021-11-21 16:07:54 +0100

    ciemny motyw

--- script.js
+++ script.js
@@ -7,6 +7,9 @@ const textera = document.querySelector(".text-feld");
 const sendBtn = document.querySelector(".send-btn");
 const warining = document.querySelector(".warning");
 const warinig2 = document.querySelector(".warning2");
+const darkmodeMain = document.querySelector('.dark-ver');
+const darkmodeBtn = document.querySelector('.fa-check-square');
+const darkmodeInfo = document.querySelector('.dark-info');
 let nameLetters = /^[a-zA-Z\s]*$/; ///[^a-zA-Z\s]*$/ - oznacza wykluczenie wszystkich znaków, /^[a-zA-Z\s]*$/ - zawiera wszystkie znaki
 let nameEmail = /^[a-z\d]+[\w\d.-]*@(?:[a-z\d]+[a-z\d-]+\.){1,5}[a-z]{2,6}$/i;
 
@@ -46,14 +49,15 @@ inputName.style.backgroundColor = 'white';
       inputEmail.style.backgroundColor = "orange";
       warinig2.textContent = 'pole "email" zawiera błędy';
     } 
-   else{
+   else if(inputName.value !== '' && inputEmail.value.match(nameEmail)){
        popupMain.style.display = 'flex'
        formMain.classList.add('blur');
        h1.classList.add('blur');
+       darkmodeMain.classList.add('blur')
        if(textera.value !== ''){
        paraPopup.textContent = `dane które przekazałeś to:\r\n twoje imię: ${inputName.value}. twój adres e-mail to: ${inputEmail.value}.\r\n a twoja wiadomość ma treść: ${textera.value}`
        } else{
-        paraPopup.textContent = `dane które przekazałeś to:\r\n twoje imię: ${inputName.value}. twój adres e-mail to: ${inputEmail.value}.\r\n niesterty nic do mnie nie napisałeś`
+        paraPopup.textContent = `dane które przekazałeś to:\r\n twoje imię: ${inputName.value}. twój adres e-mail to: ${inputEmail.value}.\r\n niestety nic do mnie nie napisałeś`
 
        }
    }
@@ -62,10 +66,22 @@ inputName.style.backgroundColor = 'white';
 };
 
 sendBtn.addEventListener("click", sendListener);
+
 popupBtn.addEventListener('click', ()=>{
     popupMain.style.display = 'none'
     formMain.classList.remove('blur');
        h1.classList.remove('blur');
+       darkmodeMain.classList.remove('blur');
        inputName.value = '';
        inputEmail.value = '';
+})
+darkmodeBtn.addEventListener('click', ()=>{
+    
+    document.body.classList.toggle('dark-mode');
+    popupMain.classList.toggle('dark-mode');
+    if(darkmodeInfo.textContent === 'ciemny motyw'){
+        darkmodeInfo.textContent = 'jasny motyw'
+    }else{
+        darkmodeInfo.textContent = 'ciemny motyw'
+    }
 })
